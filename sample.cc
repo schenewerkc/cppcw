22cfb5fb05c4b1ea015bf5d8345e7f1d7cb93ae7
Cole Schenewerk <coleschen@pc-215-65.wireless.loncourt.liberty.lan>
2014-10-24 22:30:35 +0100

    updates

--- sample.cc
+++ sample.cc
@@ -2,7 +2,8 @@
 
 sample::sample (std::vector<double> items) {
 	samples = items;
-};
+}
+
 double sample::minimum () {
 	double min=samples[0];
 	for (int i=0;i<samples.size(); ++i) {
@@ -13,6 +14,43 @@ double sample::minimum () {
 	}
 	return min;
 }
+double sample::maximum () {
+	double max=samples[0];
+	for (int i=0;i<samples.size(); ++i) {
+		if (samples[i]>max) {
+			max = samples[i];
+		}
+		else{}
+	}
+	return max;
+}
+
+double sample::range() {
+	double range = this->maximum() - this->minimum();
+	return range;
+}
+
+double sample::midrange() {
+	double midrange = (this->maximum() - this->minimum())/2;
+	return midrange;
+}
+double sample::mean () {
+	double sum = 0;
+	for (int i=0;i<samples.size(); ++i) {
+		sum = sum + samples[i];
+	}
+	double mean = sum / samples.size();
+	return mean;
+}
+double sample::variance () {
+
+}
+double sample::std_deviation () {
+
+}
+double sample::median() {
+	
+}
 
 int main () {
 	std::vector<double> vd;
@@ -21,6 +59,6 @@ int main () {
 	vd.push_back(7.2);
 
 	sample s (vd);
-	std::cout << s.minimum();
-
+	std::cout << s.minimum()<< '\n' << s.maximum() << '\n' << s.range() << '\n';
+	std::cout << s.midrange() << '\n' << s.mean() <<'\n';
 }
