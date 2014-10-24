e9bede317f6dfbbeb15a219c9335e9473430ebeb
Cole Schenewerk <coleschen@pc-215-65.wireless.loncourt.liberty.lan>
2014-10-25 00:54:52 +0100

    Implemented a few more methods

--- sample.cc
+++ sample.cc
@@ -1,4 +1,5 @@
 #include "sample.h"
+#include <cmath>
 
 sample::sample (std::vector<double> items) {
 	samples = items;
@@ -24,32 +25,62 @@ double sample::maximum () {
 	}
 	return max;
 }
-
 double sample::range() {
 	double range = this->maximum() - this->minimum();
 	return range;
 }
-
 double sample::midrange() {
 	double midrange = (this->maximum() - this->minimum())/2;
 	return midrange;
 }
 double sample::mean () {
+	//Add all values
 	double sum = 0;
 	for (int i=0;i<samples.size(); ++i) {
 		sum = sum + samples[i];
 	}
+
+	//Divide the number of values by the number of items
 	double mean = sum / samples.size();
 	return mean;
 }
 double sample::variance () {
+	std::vector<double> variances;
+	double mean = this->mean();
 
+	for (int i=0; i<samples.size(); ++i) {
+		//Compute the difference between the value and the mean
+		variances.push_back(samples[i] - mean);
+		//Square the difference
+		variances[i] = pow(variances [i],2);
+	}
+	//Average the squared values in the variances vector
+	double sum = 0;
+
+	for (int j=0; j<variances.size(); ++j) {
+		sum = sum + variances [j];
+	}
+	double variance = sum / variances.size();
+	return variance;
 }
 double sample::std_deviation () {
-
+	double std = sqrt(this->variance());
+	return std;
 }
 double sample::median() {
-	
+	std::sort (samples.begin(), samples.end());
+	double median;
+	if (samples.size() % 2 == 0) {
+		//The number of items is even
+		int index = samples.size()/2;
+		median = (samples[index] + samples[index+1])/2;
+	}
+	else {
+		//The number of items is odd
+		median = samples[((samples.size() + 1)/2)-1];
+	}
+
+	return median;
 }
 
 int main () {
@@ -57,8 +88,13 @@ int main () {
 	vd.push_back(6.0);
 	vd.push_back(4.3);
 	vd.push_back(7.2);
+	vd.push_back(8.4);
+	vd.push_back(1.4);
 
 	sample s (vd);
 	std::cout << s.minimum()<< '\n' << s.maximum() << '\n' << s.range() << '\n';
 	std::cout << s.midrange() << '\n' << s.mean() <<'\n';
+	std::cout << s.variance() << '\n' << s.std_deviation() << '\n';
+	std::cout << s.median() <<'\n';
+
 }
