ada873626a5460991c7a753bddb34558b0549e08
Raghav Narula <ragnarula87@googlemail.com>
2014-11-25 14:57:36 +0000

    generic sample header

---
+++ samplet.h
@@ -0,0 +1,206 @@
+#ifndef SAMPLE_T_H
+#define SAMPLE_T_H
+
+#include "samplet.h"
+#include <ctgmath>
+#include <string>
+#include <sstream>
+#include <istream>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+class samplet {
+    std::vector<T> _samples;
+    // static const std::regex _format;
+public:
+    samplet ();
+    samplet (const std::vector<T>&);
+
+    //Get and Set the data vector
+    const std::vector<T> & get_data() const;
+    void set_data (const std::vector<T>&);
+    void print(std::ostream&) const;
+    void read(std::istream&);
+
+    //Statistical Functions
+    T minimum() const;
+    T maximum() const;
+    T range() const;
+    T midrange() const;
+    T mean() const;
+    T variance() const;
+    T std_deviation() const;
+    T median() const;
+
+};
+
+// Overload << and >> operators for reading and printing
+template <typename T>
+std::ostream& operator<<(std::ostream&, const samplet<T>&);
+template <typename T>
+std::istream& operator>>(std::istream&,samplet<T>&);
+
+template <typename T>
+samplet<T>::samplet() 
+{
+}
+
+template <typename T>
+samplet<T>::samplet (const std::vector<T> &items) : 
+_samples(items)
+{
+    std::sort (_samples.begin(), _samples.end());
+}
+
+template <typename T>
+const std::vector<T>& samplet<T>::get_data() const
+{
+    return _samples;
+}
+
+template <typename T>
+void samplet<T>::set_data (const std::vector<T>& data)
+{
+    _samples = data;
+    std::sort (_samples.begin(), _samples.end());
+}
+
+template <typename T>
+void samplet<T>::print(ostream &os) const 
+{
+    os << "< " << _samples.size()  << ": ";
+    for(auto i = _samples.cbegin(); i != _samples.cend(); ++i){
+        os << *i << " ";
+    }
+    os << '>';
+}
+
+
+template <typename T>
+void samplet<T>::read(istream &is)
+{
+    char left, right, sep;
+    int count = 0;
+    if(is >> left){
+        if((is >> count >> sep) && left == '<' && sep == ':'){
+            while (count > 0){
+                T s;
+                is >> s;
+                _samples.push_back(s);
+                --count;
+            }
+            is >> right;
+        } else {
+            is.setstate(ios_base::badbit);
+        }
+    } 
+    std::sort (_samples.begin(), _samples.end());
+}
+
+template <typename T>
+T samplet<T>::minimum () const 
+{
+    if(_samples.empty()){
+        return 0;
+    }
+    return *_samples.begin();
+}
+
+template <typename T>
+T samplet<T>::maximum () const
+{
+    if(_samples.empty()){
+        return 0;
+    }
+    if(_samples.size() == 1){
+        return _samples[0];
+    }
+    return *(_samples.end()-1);
+}
+
+template <typename T>
+T samplet<T>::range() const
+{
+    return this->maximum() - this->minimum();
+}
+
+template <typename T>
+T samplet<T>::midrange() const
+{
+    return (this->maximum() + this->minimum())/2;
+}
+
+template <typename T>
+T samplet<T>::mean () const
+{
+    //Add all values
+    if(_samples.empty()){
+        return 0;
+    }
+    T sum = 0;
+    for (auto i = _samples.cbegin(); i != _samples.cend(); ++i) {
+        sum += *i;
+    }
+
+    //Divide the number of values by the number of items
+    return sum / _samples.size();
+}
+
+template <typename T>
+T samplet<T>::variance () const
+{
+    if(_samples.empty()){
+        return 0;
+    }
+
+    T sum;
+    int num = _samples.size();
+    for(auto i = _samples.begin(); i != _samples.end(); ++i){
+        sum += ((*i - mean())*(*i - mean()))/num;
+    }
+    return sum;
+
+}
+
+template <typename T>
+T samplet<T>::std_deviation () const
+{
+    return std::sqrt(this->variance());
+}
+
+template <typename T>
+T samplet<T>::median() const
+{
+    if(_samples.empty()){
+        return 0;
+    }
+    T median;
+    if (_samples.size() % 2 == 0) {
+        //The number of items is even
+        auto N = _samples.size()/2;
+        //container is indexed from 0, hence N-1...
+        median = (_samples[N-1] + _samples[N])/2;
+    } else {
+        //The number of items is odd
+        median = _samples[static_cast<int>(_samples.size()/2)];
+    }
+
+    return median;
+}
+// Free functions
+template <typename T>
+ostream& operator<<(std::ostream &os, const samplet<T> &s)
+{
+    s.print(os);
+    return os;
+}
+
+template <typename T>
+istream& operator>>(std::istream &is,samplet<T> &s)
+{
+    s.read(is);
+    return is;
+}
+
+#endif // SAMPLE_T_H
